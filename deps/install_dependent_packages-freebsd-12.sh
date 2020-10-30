cd6f651ea7a5fbc1d7372d6d018fe07c93e9228b
Alex Nelson <alexander.nelson@nist.gov>
2020-10-30 18:23:46 -0400

    Record testing dependencies in FreeBSD environment
    Signed-off-by: Alex Nelson <alexander.nelson@nist.gov>

---
+++ deps/install_dependent_packages-freebsd-12.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# This software was developed at the National Institute of Standards
+# and Technology by employees of the Federal Government in the course
+# of their official duties. Pursuant to title 17 Section 105 of the
+# United States Code this software is not subject to copyright
+# protection and is in the public domain. NIST assumes no
+# responsibility whatsoever for its use by other parties, and makes
+# no guarantees, expressed or implied, about its quality,
+# reliability, or any other characteristic.
+#
+# We would appreciate acknowledgement if the software is used.
+
+set -x
+set -e
+
+sudo pkg install --yes \
+  coreutils \
+  gmake \
+  openjdk11-jre \
+  py37-virtualenv \
+  wget
+
+# Guarantee /bin/bash.
+if [ ! -x /bin/bash ]; then
+  cd /bin
+    test -x /usr/local/bin/bash
+    sudo ln -s \
+      /usr/local/bin/bash \
+      bash
+  cd -
+fi
+test -x /bin/bash
