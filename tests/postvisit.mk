44fb68e119340bf6e210479a4b61828a43872a45
Alex Nelson <alexander.nelson@nist.gov>
2023-12-18 09:30:35 -0500

    Add test for IRIs that share UUIDs
    This is written in response to CASE website Issue 264.
    
    This patch is known to not pass `make check` in the current state of the
    examples.
    
    References:
    * https://github.com/casework/casework.github.io/issues/264
    
    Signed-off-by: Alex Nelson <alexander.nelson@nist.gov>

--- tests/postvisit.mk
+++ tests/postvisit.mk
@@ -49,6 +49,7 @@ all: \
 
 check: \
   check-prov-constraints \
+  check-pytest \
   kb-case_prov_check.ttl \
   undefined_concepts.txt \
   undefined_kindOfRelationships.tsv \
@@ -60,6 +61,15 @@ check-prov-constraints: \
   prov-constraints.log
 	@test 1 -eq $$(tail -n1 $< | grep 'True' | wc -l) \
 	  || (echo "ERROR:illustration.mk:prov-constraints reported a constraint error." >&2 ; exit 1)
+
+check-pytest: \
+  kb.ttl
+	source $(top_srcdir)/venv/bin/activate \
+	  && pytest \
+	    --ignore rdfs \
+	    --log-level=DEBUG \
+	    --verbose \
+	    --verbose
 
 clean:
 	@rm -f \
---
+++ tests/test_kb.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import logging
+from collections import defaultdict
+from pprint import pformat
+from typing import DefaultDict, Dict, Set
+from uuid import UUID
+
+from rdflib import Graph, URIRef
+
+
+def test_uuid_unique_usage() -> None:
+    uuid_to_urirefs: DefaultDict[UUID, Set[URIRef]] = defaultdict(set)
+    graph = Graph()
+    graph.parse("kb.ttl")
+
+    def _ingest(n_thing: URIRef) -> None:
+        thing_iri = str(n_thing)
+        if len(thing_iri) < 40:
+            # Not long enough to contain scheme, colon, and UUID.
+            return
+        try:
+            thing_uuid = UUID(thing_iri[-36:])
+        except ValueError:
+            return
+        uuid_to_urirefs[thing_uuid].add(n_thing)
+
+    for triple in graph.triples((None, None, None)):
+        if isinstance(triple[0], URIRef):
+            _ingest(triple[0])
+        if isinstance(triple[2], URIRef):
+            _ingest(triple[2])
+
+    computed: Dict[str, Set[str]] = dict()
+    for _uuid in uuid_to_urirefs:
+        if len(uuid_to_urirefs[_uuid]) > 1:
+            computed[str(_uuid)] = {str(x) for x in uuid_to_urirefs[_uuid]}
+
+    try:
+        assert len(computed) == 0
+    except AssertionError:
+        logging.debug(pformat(computed))
+        raise
